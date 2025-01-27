c125d4dc6d3269178b60e5f423ad3211d20b1841
Arjun M <lordwhitley@gmail.com>
2025-01-26 19:59:52 -0600

    ML Done

--- backend/ArtificialIntelligence/ml.py
+++ backend/ArtificialIntelligence/ml.py
@@ -1,3 +1,18 @@
+import pandas as pd
+pd.set_option('display.width', 1000)
+
+import numpy as np
+import yfinance as yf
+
+# Visualization
+import matplotlib.pyplot as plt
+
+from torchinfo import summary
+
+from sklearn.preprocessing import StandardScaler
+
+import joblib
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,64 +20,12 @@ import torch.functional as F
 from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import Dataset, DataLoader, random_split, ConcatDataset
 
-class LSTMRegression(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(LSTMRegression, self).__init__()
-        
-        self.lstm1 = nn.LSTM(input_size, 128, batch_first=True)
-        self.lstm2 = nn.LSTM(128, 256, batch_first=True)
-        self.dense1 = nn.Linear(256, 256)  
-        self.dense2 = nn.Linear(256, 128)
-        self.dense3 = nn.Linear(128, output_size)
-        
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Using device: {device}")
 
-        self.dropout = nn.Dropout(0.3)
-        
-        self.relu = nn.ReLU()
+# RL SHIT
+from typing import Optional
+import gymnasium as gym
 
-    def forward(self, x):
-        out, _ = self.lstm1(x)
-        out, _ = self.lstm2(out)
-        out = out[:, -1, :] 
-        #out = self.dropout(out)
-        
-        out = self.dense1(out)  
-        out = self.relu(out) 
-
-        out = self.dense2(out)
-        out = self.relu(out)
-
-        out = self.dense3(out)
-        
-        return out
-    
-
-class LSTMdataset(Dataset):
-    def __init__(self, data, sequence_length):
-        self.data = data
-        #print(f"Data: {self.data}")
-        #print(f"Length of Data {len(self.data)}")
-        self.data_values = self.data.values
-        self.targetIDX = data.columns.get_loc('RSI')
-        self.sequence_length = sequence_length
-        self.sample_length = sequence_length 
-        
-        
-        self.valid_indices = []
-        for i in range(len(self.data) - self.sample_length + 1): 
-            self.valid_indices.append(i)
-        #print(self.valid_indices)
-    def __len__(self):
-        return len(self.valid_indices)
-    
-    def __getitem__(self, idx):
-        if idx >= len(self.valid_indices):
-            raise IndexError("Index out of bounds")
-        
-        dayIDX = self.valid_indices[idx]
-        
-        history = self.data_values[dayIDX:dayIDX + self.sequence_length]
-
-        history = torch.tensor(history, dtype=torch.float32)
-
-        return history
+class RSITrader(gym.Env):
+    def __init__(self, )
